69e006e2e19356f9ddd0c898db0a96111a071d29
Som Shrivastava <som.shrivastava@gmail.com>
2023-07-30 13:22:02 -0400

    asdf

--- stonks.py
+++ stonks.py
@@ -39,21 +39,35 @@ Total profit = 4 + 8 = 12.
 
 class Solution:
     def stonks(self, prices):
-        #type prices: list of int
-        #return type: int
-        
-        #TODO: Write code below to returnn an int with the solution to the prompt.
-        left = 0 #Buy
-        right = 1 #Sell
-        max_profit = 0
-        while right < len(prices):
-            currentProfit = prices[right] - prices[left] #our current Profit
-            if prices[left] < prices[right]:
-                max_profit = max(currentProfit,max_profit)
+        if len(prices) < 2:
+            return 0
+        if len(prices) == 2:
+            output = prices[1] - prices[0]
+            return output if output > 0 else 0
+        i = 0
+        j = 1
+        stockBuy = prices[i]
+        stockSell = prices[j]
+        counter = 0
+        profit = 0
+        while counter < len(prices)-2:
+            if stockSell - stockBuy < 0:
+                i = counter + 1
+                j = i + 1
             else:
-                left = right
-            right += 1
-        return max_profit
+                if prices[j+1] > prices[j]:
+                    j += 1
+                else:
+                    profit = profit + (stockSell - stockBuy)
+                    i = counter + 1
+                    j = i + 1
+
+            stockSell = prices[j]
+            stockBuy = prices[i]
+            counter += 1
+        if (stockSell - stockBuy) > 0:
+            profit = profit + (stockSell - stockBuy)
+        return profit 
 
 def main():
     array = input().split(" ")
